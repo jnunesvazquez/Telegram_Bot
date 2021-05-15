dd2e5fa899449c7b990d963b545b6a648de8ace9
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-05-16 00:10:54 +0200

    sigo probando comando del dispatcher del repositorio de la biblioteca de kotlin telegram Y COMENTANDOLOS

--- src/main/kotlin/dispatcher/dispatcher.kt
+++ src/main/kotlin/dispatcher/dispatcher.kt
@@ -3,12 +3,14 @@ package dispatcher
 import Apikey.miApyKey
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.*
 import com.github.kotlintelegrambot.dispatcher.message
-import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
-import com.github.kotlintelegrambot.entities.KeyboardReplyMarkup
-import com.github.kotlintelegrambot.entities.ParseMode
+import com.github.kotlintelegrambot.entities.*
+import com.github.kotlintelegrambot.entities.dice.DiceEmoji
+import com.github.kotlintelegrambot.entities.inlinequeryresults.InlineQueryResult
+import com.github.kotlintelegrambot.entities.inlinequeryresults.InputMessageContent
+import com.github.kotlintelegrambot.entities.inputmedia.InputMediaPhoto
+import com.github.kotlintelegrambot.entities.inputmedia.MediaGroup
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import com.github.kotlintelegrambot.entities.keyboard.KeyboardButton
 import com.github.kotlintelegrambot.extensions.filters.Filter
@@ -21,12 +23,13 @@ fun main() {
         token = miApyKey
 
         dispatch {
-
+            //Este mensaje salta cuando contestas al bot con un stiker
             message(Filter.Sticker) {
                 bot.sendMessage(ChatId.fromId(message.chat.id), text = "You have received an awesome sticker \\o/")
             }
+            //Este mensaje salta cuando reenvias el mensaje del bot o por que lo respondes en el chat
             message(Filter.Reply or Filter.Forward) {
-                bot.sendMessage(ChatId.fromId(message.chat.id), text = "someone is replying or forwarding messages ...")
+                bot.sendMessage(ChatId.fromId(message.chat.id), text = "Damian, tienes que aprobar a Joel y Brais")
             }
 
             command("start") {
@@ -110,11 +113,13 @@ fun main() {
                     parseMode = ParseMode.MARKDOWN_V2
                 )
             }
-
+            /*
+            sirve para crear dos botones
+             */
             command("inlinebuttons") {
                 val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
-                    listOf(InlineKeyboardButton.CallbackData(text = "Test Inline Button", callbackData = "testButton")),
-                    listOf(InlineKeyboardButton.CallbackData(text = "Show alert", callbackData = "showAlert"))
+                    listOf(InlineKeyboardButton.CallbackData(text = "Presioname", callbackData = "Madre mia")),
+                    listOf(InlineKeyboardButton.CallbackData(text = "hola", callbackData = "showAlert"))
                 )
                 bot.sendMessage(
                     chatId = ChatId.fromId(message.chat.id),
@@ -134,7 +139,100 @@ fun main() {
                     replyMarkup = keyboardMarkup
                 )
             }
+            /*
+            Este comando sirve para poder enviar fotos con el bot
+             */
+            command("mediagroup") {
+                bot.sendMediaGroup(
+                    chatId = ChatId.fromId(message.chat.id),
+                    mediaGroup = MediaGroup.from(
+                        InputMediaPhoto(
+                            media = TelegramFile.ByUrl("https://www.wallpapertip.com/wmimgs/67-674353_epic-anime-wallpapers-21118-hd-pictures-data-src.jpg"),
+                            caption = "Asombrate de mis fotos Damian"
+                        ),
+                        InputMediaPhoto(
+                            media = TelegramFile.ByUrl("https://images5.alphacoders.com/105/thumb-1920-1055769.jpg"),
+                            caption = "Final Space best serie"
+                        )
+                    ),
+                    replyToMessageId = message.messageId
+                )
+            }
+            //este texto de entrada tiene que coincidir con el texto que se le ponga al boton EN callbackData
+            callbackQuery("Madre mia") {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), callbackQuery.data)
+            }
+            //este texto de entrada tiene que coincidir con el texto que se le ponga al boton EN callbackData
+            callbackQuery(
+                callbackData = "showAlert",
+                callbackAnswerText = "HelloText",
+                callbackAnswerShowAlert = true
+            ) {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), callbackQuery.data)
+            }
+
+            text("ping") {
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Pong")
+            }
+
+            location {
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Your location is (${location.latitude}, ${location.longitude})",
+                    replyMarkup = ReplyKeyboardRemove()
+                )
+            }
+
+            contact {
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Hello, ${contact.firstName} ${contact.lastName}",
+                    replyMarkup = ReplyKeyboardRemove()
+                )
+            }
+
+            channel {
+                // Handle channel update
+            }
+
+            inlineQuery {
+                val queryText = inlineQuery.query
+
+                if (queryText.isBlank() or queryText.isEmpty()) return@inlineQuery
+
+                val inlineResults = (0 until 5).map {
+                    InlineQueryResult.Article(
+                        id = it.toString(),
+                        title = "$it. $queryText",
+                        inputMessageContent = InputMessageContent.Text("$it. $queryText"),
+                        description = "Add $it. before you word"
+                    )
+                }
+                bot.answerInlineQuery(inlineQuery.id, inlineResults)
+            }
+
+            photos {
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Wowww, awesome photos!!! :P"
+                )
+            }
+
+            command("diceAsDartboard") {
+                bot.sendDice(ChatId.fromId(message.chat.id), DiceEmoji.Dartboard)
+            }
+
+            dice {
+                bot.sendMessage(ChatId.fromId(message.chat.id), "A dice ${dice.emoji.emojiValue} with value ${dice.value} has been received!")
+            }
+
+            telegramError {
+                println(error.getErrorMessage())
+            }
         }
+
     }
     bot.startPolling()
 
@@ -145,4 +243,3 @@ fun generateUsersButton(): List<List<KeyboardButton>> {
         listOf(KeyboardButton("Request contact", requestContact = true))
     )
 }
-
