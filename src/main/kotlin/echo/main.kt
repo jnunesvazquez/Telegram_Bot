fd198639106cf45e9740fe356917dc8f233c0bf1
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-05-16 19:29:26 +0200

    # Conflicts: #	src/main/kotlin/echo/main.kt

--- src/main/kotlin/echo/main.kt
+++ src/main/kotlin/echo/main.kt
@@ -6,12 +6,17 @@ import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.network.fold
 
 fun main() {
+
+    //Instanciamos un objeto bot que a su vez instanciaremos con un builder
     val bot = bot {
 
+        //Introducimos el token de nuestro bot
         token = miApyKey
 
+        //El dispatch nos permitira enviar información a la api de Telegram que manejara nuestro bot
         dispatch {
 
+            //Creamos un comando que devolvera un mensaje que nosotros indiquemos
             command("start") {
 
                 val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Bot iniciado")
